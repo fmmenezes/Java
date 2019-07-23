c8ab4e94bac78aaa67a5976896a26e46cf0e48c1
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-07-22 22:58:36 -0300

    Inclusão do flagDeposito

--- aula62/src/application/Program.java
+++ aula62/src/application/Program.java
@@ -3,22 +3,38 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.ContaCorrente;
+
 class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-				
+
+		ContaCorrente cc = new ContaCorrente();
+		
+		double valorDeposito = 0.00;
+
 		System.out.print("Informe o numero da conta: ");
 		int conta = sc.nextInt();
+		cc.conta = conta;
 		System.out.print("Informe o nome do titular: ");
 		String titular = sc.next();
+		cc.titular = titular;
 		System.out.print("Existe deposito inicial (y/n)? ");
-		String flagDeposito = sc.next();
-		System.out.print("Infore o valor do deposito: ");
-		double valorDeposito = sc.nextDouble();
 		
+		char flagDeposito = sc.next().charAt(0);
+		if (flagDeposito == 'Y') {
+			
+			System.out.print("Informe o valor do deposito: ");
+			valorDeposito = sc.nextDouble();
+			cc.deposito(valorDeposito);
+		}
+		
+		System.out.println();
+		System.out.println(cc);
+
 		sc.close();
 
 	}
