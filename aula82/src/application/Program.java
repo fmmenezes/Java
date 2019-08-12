88165a47ead2494c52432bfa2a967ebca689407f
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-08-12 20:15:17 -0300

    Exemplo Calendar

--- aula82/src/application/Program.java
+++ aula82/src/application/Program.java
@@ -1,10 +1,35 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		System.out.println(sdf.format(d));
+		
+		Calendar cal = Calendar.getInstance();
+		
+		cal.setTime(d);
+		
+		//cal.add(Calendar.HOUR_OF_DAY, 4);
+		//d = cal.getTime();
+		
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = cal.get(Calendar.MONTH) + 1;
+		
+		//System.out.println(sdf.format(d));
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Month: " + month);
 
+		
+		
 	}
 
 }
