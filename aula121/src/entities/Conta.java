ef994043c8dd363f96a89f94a5fe245d93ac2cc7
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-09-11 21:23:34 -0300

    Implement - Classe Conta

---
+++ aula121/src/entities/Conta.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Conta {
+	private Integer numero;
+	private String titular;
+	private Double Saldo;
+	
+	public Conta() {
+		
+	}
+
+	public Conta(Integer numero, String titular, Double saldo) {
+		this.numero = numero;
+		this.titular = titular;
+		this.Saldo = saldo;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public Double getSaldo() {
+		return Saldo;
+	}
+
+	//public void setSaldo(Double saldo) {
+	//	Saldo = saldo;
+	//}
+	
+	public void Saque(Double valor) {
+		Saldo -= valor;
+	}
+	
+	public void Deposito(Double valor) {
+		Saldo += valor;
+	}
+	
+}
