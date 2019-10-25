838e8c531be122c44551f0692590bcb9d60d814a
Fernando Mendes de Menezes <fmmenezes@hotmail.com>
2019-10-24 22:34:43 -0300

    Exemplo - FileReader e BufferedReader.

--- aula175/src/application/Program.java
+++ aula175/src/application/Program.java
@@ -1,9 +1,27 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		File arquivo = new File("C:\\temp\\arquivoTeste.txt");
+		Scanner sc = null;
+		try {
+		sc = new Scanner(arquivo);
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+		}catch(IOException e){
+			System.out.println("Erro: " + e.getMessage());
+			
+		}finally {
+			sc.close();
+		}
 
 	}
 
