004dc22cea24b1eefccec95e112541526aaa78c9
fermende <fermende@NTB128098.corp.capgemini.com>
2020-01-03 15:18:01 -0300

    Inclusao do metodo processaFatura.

--- 184Interfaces/src/model/services/ServicoAluguel.java
+++ 184Interfaces/src/model/services/ServicoAluguel.java
@@ -1,6 +1,7 @@
 package model.services;
 
 import model.entities.Aluguel;
+import model.entities.Fatura;
 
 public class ServicoAluguel {
 	
@@ -21,6 +22,23 @@ public class ServicoAluguel {
 	}
 	
 	public void processaFatura(Aluguel aluguel) {
+		
+		long dataInicioMs = aluguel.getInicio().getTime();
+		long dataFimMs = aluguel.getFim().getTime();
+		Double valorPagamento;
+		
+		double horas = (double)(dataFimMs - dataInicioMs) / 1000 / 60 / 60; 
+		
+		if (horas <= 12.0) {
+			valorPagamento = Math.ceil(horas) * precoPorHora;
+		}
+		else {
+			valorPagamento = Math.ceil(horas / 24) * precoPorDia;
+		}
+		
+		double taxa = servicoTaxa.taxa(valorPagamento);
+		
+		aluguel.setFatura(new Fatura(valorPagamento, taxa));
 		
 	}
 	
