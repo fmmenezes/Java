9f5406c2932eb3bdef91bcf1a6df1f2ab168513e
fermende <fermende@NTB128098.corp.capgemini.com>
2020-01-21 19:32:15 -0300

    Classe Contrato - Inclusao de propriedades e metodos

--- 189ExemploInterface/src/entities/Contrato.java
+++ 189ExemploInterface/src/entities/Contrato.java
@@ -1,5 +1,45 @@
 package entities;
 
+import java.util.Date;
+
 public class Contrato {
+	
+	private Integer numeroContrato;
+	private Date dataContrato;
+	private Double valorTotalContrato;
+	
+	public Contrato(Integer numeroContrato, Date dataContrato, Double valorTotalContrato) {
+		this.numeroContrato = numeroContrato;
+		this.dataContrato = dataContrato;
+		this.valorTotalContrato = valorTotalContrato;
+	}
+
+	public Integer getNumeroContrato() {
+		return numeroContrato;
+	}
+
+	public void setNumeroContrato(Integer numeroContrato) {
+		this.numeroContrato = numeroContrato;
+	}
+
+	public Date getDataContrato() {
+		return dataContrato;
+	}
+
+	public void setDataContrato(Date dataContrato) {
+		this.dataContrato = dataContrato;
+	}
+
+	public Double getValorTotalContrato() {
+		return valorTotalContrato;
+	}
+
+	public void setValorTotalContrato(Double valorTotalContrato) {
+		this.valorTotalContrato = valorTotalContrato;
+	}
+	
+	
+	
+	
 
 }
