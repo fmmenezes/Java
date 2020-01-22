36b115692df723e1456bdf31da0cb5dabb5fbeba
fermende <fermende@NTB128098.corp.capgemini.com>
2020-01-22 13:39:00 -0300

    Classe Paecela - Inclusao de propriedades e metodos.

--- 189ExemploInterface/src/entities/Parcela.java
+++ 189ExemploInterface/src/entities/Parcela.java
@@ -1,5 +1,41 @@
 package entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Parcela {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	private Date dataParcela;
+	private Double valorParcela;
+	
+	public Parcela(Date dataParcela, Double valorParcela) {
+		
+		this.dataParcela = dataParcela;
+		this.valorParcela = valorParcela;
+	}
+
+	public Date getDataParcela() {
+		return dataParcela;
+	}
+
+	public void setDataParcela(Date dataParcela) {
+		this.dataParcela = dataParcela;
+	}
+
+	public Double getValorParcela() {
+		return valorParcela;
+	}
+
+	public void setValorParcela(Double valorParcela) {
+		this.valorParcela = valorParcela;
+	}
+	
+	@Override
+	public String toString() {
+		return sdf.format(dataParcela) + " - " + String.format("%.2f", valorParcela);
+	}
+	
 
 }
