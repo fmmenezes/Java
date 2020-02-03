d6650384068168b24899fef2d9ebf00d9d9e3d77
fermende <fermende@NTB128098.corp.capgemini.com>
2020-02-03 12:14:47 -0300

    Inclusão da classe Empregado.

---
+++ 192InterfaceComparable/src/entities/Empregado.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Empregado {
+	
+	private String nome;
+	private Double salario;
+	
+	public Empregado(String nome, Double salario) {
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
+	
+
+}
