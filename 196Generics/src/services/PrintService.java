bf03ad2d0bbae551da2f0c0a6a80874f7ff8520f
fermende <fermende@NTB128098.corp.capgemini.com>
2020-02-05 15:04:54 -0300

    Implementação inicial.

--- 196Generics/src/services/PrintService.java
+++ 196Generics/src/services/PrintService.java
@@ -1,5 +1,32 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintService {
+	
+	private List<Integer> lista = new ArrayList<>();
+	
+	public void adicionaValor(Integer valor) {
+		lista.add(valor);
+	}
+	
+	public Integer first() {
+		if(lista.isEmpty()) {
+			throw new IllegalStateException("A lista está vazia!");
+		}
+		return lista.get(0);
+	}
+	
+	public void print() {
+		System.out.println("[");
+		if (!lista.isEmpty()) {
+			System.out.print(lista.get(0));
+		}
+		for (int i = 1; i < lista.size(); i++ ) {
+			System.out.println(", " + lista.get(i));
+		}
+		System.out.println("]");
+	}
 
 }
